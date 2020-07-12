6818bd76463afe6f883b0834846ae9cc4479bf00
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-12 11:09:07 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   def initialize(title)
     @title = title
   end
-
+  def genre=(gend)
+    @genre=gend  
+  end  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
